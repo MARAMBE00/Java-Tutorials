2b048fe265993cd98b2d69ad568eb99c0d131620
Pasindu_Marambe <pasindurmarambe00@gmail.com>
2023-06-10 13:25:03 +0530

    Tutorial_4

---
+++ Tutorial_4/q3b.java
@@ -0,0 +1,12 @@
+public class q3b{
+    public static void main(String[] args) {
+
+        double total = 0;
+        total = multiply(3, 2);
+        System.out.println(total);
+    }
+
+    private static double multiply(int sum1, int sum2){
+        return (double) sum1 * sum2;
+    }
+}
